f60e51bc6a34d89079e62c5f0b7c2e48a919a3cd
Shashank Date <stdate@gmail.com>
2025-06-23 14:09:48 -0400

    Add condition to the Perplexity Prompt

--- examples/perplexity_api.py
+++ examples/perplexity_api.py
@@ -8,7 +8,7 @@ client = OpenAI(
     base_url="https://api.perplexity.ai"
 )
 
-prompt = "Extract all events from this page. For each event, return the name, date, location, and description from: https://www.torontotechweek.com/calendar \nList the events in CSV format with no other extra verbiage befor or after the list."
+prompt = "Extract ALL events from this page which satisfy the condition: 'INTEREST = Career Fair, DATE = 25 June 2025'. For each event, return the name, date, location, and description from: https://www.torontotechweek.com/calendar \nList the events in CSV format with no other extra verbiage befor or after the list."
 
 response = client.chat.completions.create(
     model="sonar-pro",

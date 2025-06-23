194d8a479e30066a3e7a41181886db1916691c50
Shashank Date <stdate@gmail.com>
2025-06-23 12:45:25 -0400

    Modify elevenlabs example to do STT

--- examples/eleven_labs.py
+++ examples/eleven_labs.py
@@ -1,5 +1,9 @@
 import os
+import pprint
+import re
 from dotenv import load_dotenv
+from io import BytesIO
+import requests
 from elevenlabs.client import ElevenLabs
 from elevenlabs import play
 
@@ -16,6 +20,29 @@ audio = elevenlabs.text_to_speech.convert(
     output_format="mp3_44100_128",
 )
 
-play(audio)
+play(audio) ## TTS
 
+audio_url = (
+    "https://storage.googleapis.com/eleven-public-cdn/audio/marketing/nicole.mp3"
+)
+response = requests.get(audio_url)
+audio_data = BytesIO(response.content)
 
+transcription = elevenlabs.speech_to_text.convert(
+    file=audio_data,
+    model_id="scribe_v1", # Model to use, for now only "scribe_v1" is supported
+    tag_audio_events=True, # Tag audio events like laughter, applause, etc.
+    language_code="eng", # Language of the audio file. If set to None, the model will detect the language automatically.
+    diarize=True, # Whether to annotate who is speaking
+)
+
+# pprint.pp(transcription.words)
+
+# Print only the text for each word in the transcription.words array
+txt = ""
+for word in transcription.words:
+    txt += word.text
+
+# Remove contiguous whitespace from the txt string
+txt = re.sub(r'\s+', ' ', txt).strip()
+print(txt)
